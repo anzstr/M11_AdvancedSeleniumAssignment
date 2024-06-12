ac03801852a7fb461fec36955bed764bc091a8b9
Angela S <a.strunnikova@gmail.com>
2024-06-11 21:29:54 -0400

    Test Months droplist

--- src/test/java/FacebookSignupPageTest.java
+++ src/test/java/FacebookSignupPageTest.java
@@ -3,13 +3,16 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.ParameterizedTest;
 //import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -33,17 +36,6 @@ public class FacebookSignupPageTest {
     @AfterEach
     public void testTeardown() {
         driver.get(HOME_PAGE_URL);
-    }
-
-    @Test
-    public void gendersTest() throws InterruptedException {
-        String femaleXpath = "//*[@name='sex' and @value=1]";
-        String maleXpath = "//*[@name='sex' and @value=2]";
-
-        driver.findElement(By.xpath("//*[text()='Create new account']")).click();
-        assertNotNull(driver.findElement(By.xpath("//*[text()='Sign Up']")));
-
-        Thread.sleep(1000);
     }
 
     @Test
@@ -85,8 +77,37 @@ public class FacebookSignupPageTest {
         driver.findElement(By.xpath("//*[@name='reg_passwd__']")).click();
         WebElement errorMsgPassword = driver.findElement(By.xpath("//*[contains(text(), 'combination')]"));
         assertNotNull(errorMsgPassword);
+    }
 
+    @ParameterizedTest
+    @MethodSource("dataProvider")
+    public void monthTest(String monthInputName, String monthInputValue) throws InterruptedException {
+        driver.findElement(By.xpath("//*[text()='Create new account']")).click();
+        assertNotNull(driver.findElement(By.xpath("//*[text()='Sign Up']")));
         Thread.sleep(3000);
+
+        driver.findElement(By.xpath("//*[@title='Month']")).click();
+        driver.findElement(By.xpath("//*[text() = '" + monthInputName + "']")).click();
+        String monthValue = driver.findElement(By.xpath("//*[@title='Month']")).getAttribute("value");
+
+        assertEquals(monthInputValue, monthValue);
+    }
+
+    static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                Arguments.of("Jan", "1"),
+                Arguments.of("Feb", "2"),
+                Arguments.of("Mar", "3"),
+                Arguments.of("Apr", "4"),
+                Arguments.of("May", "5"),
+                Arguments.of("Jun", "6"),
+                Arguments.of("Jul", "7"),
+                Arguments.of("Aug", "8"),
+                Arguments.of("Sep", "9"),
+                Arguments.of("Oct", "10"),
+                Arguments.of("Nov", "11"),
+                Arguments.of("Dec", "12")
+        );
     }
 
 }
